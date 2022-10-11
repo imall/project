022cb42e274fe3add6479e82b06276d36ca21a9b
imall <imall841110@gmail.com>
2022-10-11 16:48:15 +0800

    'add'

--- nuxt/server/chunks/index-styles.70b9fecb.mjs
+++
@@ -1,6 +0,0 @@
-import { a as index_vue_vue_type_style_index_0_scoped_a23d179e_lang } from './styles.mjs';
-
-const indexStyles_70b9fecb = [index_vue_vue_type_style_index_0_scoped_a23d179e_lang];
-
-export { indexStyles_70b9fecb as default };
-//# sourceMappingURL=index-styles.70b9fecb.mjs.map
